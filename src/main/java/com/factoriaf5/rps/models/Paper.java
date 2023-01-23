85c8916b157c9afc6c8e3a470c268ebb618dc9a2
Enol-Igareta <enol1code@gmail.com>
2023-01-23 10:24:06 +0100

    primera iteraccion acabada

--- src/main/java/com/factoriaf5/rps/models/Paper.java
+++ src/main/java/com/factoriaf5/rps/models/Paper.java
@@ -1,5 +1,39 @@
 package com.factoriaf5.rps.models;
 
 public class Paper {
-    
+
+    private boolean alive = true;
+    private String type = "paper";
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Paper() {
+
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public void attackRockFromPaper(Rock rock){
+        if(rock.getType() == "rock"){
+            rock.setAlive(false);
+        }
+    }
+
+    public void attackScissorsFromPaper(Scissors scissors){
+        if(scissors.getType() == "scissors"){
+            this.setAlive(false);
+        }
+    }
 }
--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -4,10 +4,62 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.factoriaf5.rps.models.Paper;
+import com.factoriaf5.rps.models.Rock;
+import com.factoriaf5.rps.models.Scissors;
+
 public class GameTest {
     
     @Test
-    public void justASimpleTest() {
-        assertTrue(true);
+    public void paper_win_vs_rock() {
+        Rock rock = new Rock();
+        Paper paper = new Paper();
+        paper.attackRockFromPaper(rock);
+        assertEquals(true,paper.isAlive());
+        assertEquals(false,rock.isAlive());
+    }
+    @Test
+    public void paper_loos_vs_scissors() {
+        Scissors scissors = new Scissors();
+        Paper paper = new Paper();
+        paper.attackScissorsFromPaper(scissors);
+        assertEquals(false,paper.isAlive());
+        assertEquals(true,scissors.isAlive());
+    }
+
+    @Test
+    public void scissors_loos_vs_rock() {
+        Scissors scissors = new Scissors();
+        Rock rock = new Rock();
+        scissors.attackRockFromScissors(rock);
+        assertEquals(false,scissors.isAlive());
+        assertEquals(true,rock.isAlive());
+    }
+
+    @Test
+    public void sicssors_win_vs_paper() {
+        Scissors scissors = new Scissors();
+        Paper paper = new Paper();
+        scissors.attackPaperFromScissors(paper);
+        assertEquals(false,paper.isAlive());
+        assertEquals(true,scissors.isAlive());
+    }
+
+    @Test
+    public void rock_loos_vs_paper() {
+        Rock rock = new Rock();
+        Paper paper = new Paper();
+        rock.attackPaperFromRock(paper);
+        assertEquals(true, paper.isAlive());
+        assertEquals(false, rock.isAlive());
+    }
+
+    @Test
+    public void rock_win_vs_scissors() {
+        Scissors scissors = new Scissors();
+        Rock rock = new Rock();
+        rock.attackScissorsFromRock(scissors);
+        assertEquals(false,scissors.isAlive());
+        assertEquals(true,rock.isAlive());
     }
 }
